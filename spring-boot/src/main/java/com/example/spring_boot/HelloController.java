27cda01aa6e411720676eb48fc6838937ce5a403
aaravbaphna <65755447+aaravbaphna@users.noreply.github.com>
2025-05-07 17:26:26 -0700

    Add files via upload

---
+++ spring-boot/src/main/java/com/example/spring_boot/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.spring_boot;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public Resource homePage() {
+        return new ClassPathResource("index.html");
+    }
+}
